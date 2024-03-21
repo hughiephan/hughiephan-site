11108476c5c3b11db8e87c5a1facff166be5e26f
DESKTOP-UJVBOBB\phant <phanthanhhuy1996@gmail.com>
2024-03-21 22:56:09 +0700

    no message

--- src/views/inner-pages/features/miscellaneous/Research.jsx
+++ src/views/inner-pages/features/miscellaneous/Research.jsx
@@ -87,6 +87,13 @@ const Research = () => {
                             }
                             <span> </span> <span> </span>
 
+                            {research.student &&
+                              <a href={research.student} target="_blank" rel="noreferrer">
+                                <i className="fa fa-file-o"> Student Proposal</i>
+                              </a>
+                            }
+                            <span> </span> <span> </span>
+
                             {research.paper &&
                               <a href={research.paper} target="_blank" rel="noreferrer">
                                 <i className="fa fa-book"> Paper</i>
@@ -124,6 +131,13 @@ const Research = () => {
                             }
                             <span> </span> <span> </span>
 
+                            {research.student &&
+                              <a href={research.student} target="_blank" rel="noreferrer">
+                                <i className="fa fa-file-o"> Student Proposal</i>
+                              </a>
+                            }
+                            <span> </span> <span> </span>
+
                             {research.paper &&
                               <a href={research.paper} target="_blank" rel="noreferrer">
                                 <i className="fa fa-book"> Paper</i>
@@ -148,6 +162,41 @@ const Research = () => {
                         <h3> {research.title} </h3>
                         <div className="update-date"> {research.author} </div>
                         <p> {research.description} </p>
+
+                        {(research.proposal || research.paper || research.report) &&
+                          <div>
+                            Detail (access required):
+                            <span> </span>
+
+                            {research.proposal &&
+                              <a href={research.proposal} target="_blank" rel="noreferrer">
+                                <i className="fa fa-file-text-o"> Proposal</i>
+                              </a>
+                            }
+                            <span> </span> <span> </span>
+                            
+                            {research.student &&
+                              <a href={research.student} target="_blank" rel="noreferrer">
+                                <i className="fa fa-file-o"> Student Proposal</i>
+                              </a>
+                            }
+                            <span> </span> <span> </span>
+
+                            {research.paper &&
+                              <a href={research.paper} target="_blank" rel="noreferrer">
+                                <i className="fa fa-book"> Paper</i>
+                              </a>
+                            }
+                            <span> </span> <span> </span>
+
+                            {research.report &&
+                              <a href={research.report} target="_blank" rel="noreferrer">
+                                <i className="fa fa-pencil-square-o"> Report</i>
+                              </a>
+                            }
+                          </div>
+                        }
+
                         {research.link && research.link.map((link, i) => (
                           <a href={link}> {link} </a>
                         ))}
