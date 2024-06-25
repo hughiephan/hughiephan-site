e91b8ef156cc18d7a6575dba0abfa88ad4012755
DESKTOP-UJVBOBB\phant <phanthanhhuy1996@gmail.com>
2024-06-25 18:37:11 +0700

    no message

--- src/components/contact/address/ContactAddress.jsx
+++ src/components/contact/address/ContactAddress.jsx
@@ -18,13 +18,13 @@ const socialContent = [
 const ContactAddress = () => {
   return (
     <div className="row justify-content-center">
-      <div className="col-lg-4 col-sm-6 d-lg-flex">
+      <div className="col-lg-3 col-sm-5 d-lg-flex">
         <div className="address-info">
           <div className="icon d-flex align-items-end">
             <img src="images/icon/44.svg" alt="icon" />
           </div>
           <div className="title">Location</div>
-          <p className="font-rubik">
+          <p style={{fontSize: "25px"}} className="font-rubik">
             Vietnam,  <br /> Ho Chi Minh city
           </p>
         </div>{" "}
@@ -32,13 +32,13 @@ const ContactAddress = () => {
       </div>
       {/* End .col */}
 
-      <div className="col-lg-4 col-sm-6 d-lg-flex">
+      <div className="col-lg-3 col-sm-5 d-lg-flex">
         <div className="address-info">
           <div className="icon d-flex align-items-end">
             <img src="images/icon/45.svg" alt="icon" />
           </div>
           <div className="title">Contact</div>
-          <p className="font-rubik">
+          <p style={{fontSize: "25px"}}className="font-rubik">
             @Gmail <br />
             phanthanhhuy1996
           </p>
@@ -47,13 +47,13 @@ const ContactAddress = () => {
       </div>
       {/* End .col */}
 
-      <div className="col-lg-4 col-sm-6 d-lg-flex">
+      <div className="col-lg-3 col-sm-5 d-lg-flex">
         <div className="address-info">
           <div className="icon d-flex align-items-end">
             <img src="images/icon/46.svg" alt="icon" />
           </div>
           <div className="title">Social Media</div>
-          <p className="font-rubik">Find on social media</p>
+          <p style={{fontSize: "25px"}} className="font-rubik">Find on</p>
           <ul className="d-flex justify-content-center">
               <li>
                 <a href="https://www.facebook.com/groups/hughiephan" target="_blank" rel="noreferrer">
@@ -85,6 +85,24 @@ const ContactAddress = () => {
         {/* /.address-info  */}
       </div>
       {/* End .col */}
+
+      
+      <div className="col-lg-3 col-sm-5 d-lg-flex">
+        <div className="address-info">
+          <div className="icon d-flex align-items-end">
+            <img src="images/icon/13.svg" alt="icon" />
+          </div>
+          <div className="title">Academics</div>
+          <div>
+          <img style={{width: "50px", display: "inline-block"}} src="https://scontent.fsgn2-7.fna.fbcdn.net/v/t39.30808-6/442444619_1128568458385732_356286600298876385_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=ePrxf_VTwnsQ7kNvgHXVNea&_nc_ht=scontent.fsgn2-7.fna&oh=00_AYClp241t7SZwZ0fTrIAj2I1F6nwaLhhcdj0nfHQeH88TA&oe=66808E84"></img>
+          <img style={{width: "100px", display: "inline-block"}} src="https://www.reloadconsulting.com/wp-content/uploads/2014/11/Copy-of-DTESB-Logo-20.png"></img>
+        
+          </div>
+          </div>{" "}
+        {/* /.address-info  */}
+      </div>
+      {/* End .col */}
+
     </div>
   );
 };
