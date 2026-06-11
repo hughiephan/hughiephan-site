11dbfd6775c13c9bff74346778a31c267da3100e
HUGHIE-PC\Hughie <phanthanhhuy1996@gmail.com>
2026-06-11 20:15:29 +1000

    no message

--- src/views/inner-pages/features/miscellaneous/ResearchTab.jsx
+++ src/views/inner-pages/features/miscellaneous/ResearchTab.jsx
@@ -191,24 +191,10 @@ const ResearchTab = (props) => {
                 <div className={getClassName(research, "research-tab", "literature")}>
                     <img style={{ opacity: "85%", paddingRight: "5px", width: "30px" }} src="images/icon/84.svg" alt="media" />
                     <div className="content">
-                        <nav className="navbar">
-                            <li style={{ listStyleType: "none" }} className="dropdown">
-                                <a className="nav-link dropdown-toggle" data-toggle="dropdown">
-                                    Literature
-                                </a>
-                                <ul className="dropdown-menu">
-                                    {research.literature.map((p, index) => (
-                                        <li key={index} style={{ listStyleType: "none" }}>
-                                            <a href={p.url}> {p.label}</a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </li>
-                        </nav>
+                        <a href={research.literature}>Literature</a>
                     </div>
                 </div>
             }
-
 
 
             {!research.presentation &&
